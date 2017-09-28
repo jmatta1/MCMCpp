f7a1d8ae587dd59809ae26268c4758b24a6e5b72
James Till Matta <jamesmatta@gmail.com>
2017-09-28 17:36:46 -0400

    More fully documented the iterators
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- Chain/ChainPsetIterator.h
+++ Chain/ChainPsetIterator.h
@@ -32,21 +32,21 @@ namespace Chain
  * \tparam ParamType The floating point type to be used for the chain, float, double, long double, etc.
  * \tparam BlockSize The number of steps per walker that the block will hold
  * 
- * ChainStepIterator allows traversal of the chain step by step. That is
- * to say that incrementing the iterator jumps all the parameter sets of all the
- * walkers, which is useful for operations that need to look at all the walkers
- * in a given point, at once.
+ * ChainPsetIterator allows traversal point by point. That
+ * is to say that incrementing the iterator moves to the next parameter set,
+ * which could be in the same step just the next walker, or it could be in the
+ * first walker of the next step.
  * 
- * ChainStep is a BidirectionalIterator, you can increment and decrement it, as
- * well as dereference it. However, you must use prefix increment and decrement
- * post-fix increment and decrement are not supported.
+ * ChainPsetIterator is a BidirectionalIterator, you can increment and decrement
+ * it, as well as dereference it. However, you must use prefix increment and
+ * decrement post-fix increment and decrement are not supported.
  */
 template <class ParamType, int BlockSize>
 class ChainPsetIterator
 {
 public:
     /*!
-     * \brief Chain Constructor
+     * \brief ChainPsetIterator Constructor
      * \param block The block the iterator is starting pointed to
      * \param blockStep The step within the block that the iterator starts pointing to
      */
@@ -54,12 +54,35 @@ public:
         curr(block), stepIndex(blockStep), lastCell(BlockSize*block->walkerCount - 1){}
     ~ChainPsetIterator(){}
     
+    /*!
+     * \brief operator== equality operator to test equality of two iterators (equivalence of their locations)
+     * \param rhs the second iterator (the right hand side)
+     * \return  True if they are equal, false otherwise
+     */
     bool operator==(const ChainStepIterator& rhs){return ((curr == rhs.curr) && (index == rhs.index));}
+    
+    /*!
+     * \brief operator!= equality operator to test inequality of two iterators (inequivalence of their locations)
+     * \param rhs the second iterator (the right hand side)
+     * \return  True if they are not equal, false otherwise
+     */
     bool operator!=(const ChainStepIterator& rhs){return ((curr != rhs.curr) || (index != rhs.index));}
     
+    /*!
+     * \brief operator++ Prefix increment of the iterator, move it to the next walker parameter set in the chain
+     * \return The iterator that was incremented
+     */
     ChainPsetIterator<ParamType, BlockSize> operator++();
+    /*!
+     * \brief operator++ Prefix decrement of the iterator, move it to the previous walker parameter set in the chain
+     * \return The iterator that was decremented
+     */
     ChainPsetIterator<ParamType, BlockSize> operator--();
     
+    /*!
+     * \brief operator* Dereference the iterator to get a pointer to the walker parameter array for this walker parameter set
+     * \return A pointer into the walker parameter array, pointed to the beginning of the current walker's parameter set
+     */
     ParamType* operator*(){return (curr->chainArray + index*(curr->cellsPerWalker));}
     
 private:
@@ -72,6 +95,13 @@ private:
 template <class ParamType, int BlockSize>
 ChainPsetIterator<ParamType, BlockSize> ChainPsetIterator<ParamType, BlockSize>::operator++()
 {
+    /*!
+     * @remark This function will allow the iterator to be incremented past the
+     * end. In fact, if the end is not the last cell of the last block merely a
+     * a cell in the last block, then it is possible to increment many times past
+     * the end iterator, once the iterator has gone past the last cell of the
+     * last block, the iterator is not recoverable
+     */
     //check if we are not at the end of a block
     if(index < lastCell)
     {//not at the end of a block, easy peasy
@@ -88,6 +118,10 @@ ChainPsetIterator<ParamType, BlockSize> ChainPsetIterator<ParamType, BlockSize>:
 template <class ParamType, int BlockSize>
 ChainPsetIterator<ParamType, BlockSize> ChainPsetIterator<ParamType, BlockSize>::operator--()
 {
+    /*!
+     * @remark This function will allow a .begin() operator to be decremented,
+     * once this happens the iterator is not recoverable
+     */
     //check if we are not at the end of a block
     if(index > 0)
     {//not at the start of a block, easy peasy
--- Chain/ChainStepIterator.h
+++ Chain/ChainStepIterator.h
@@ -37,28 +37,50 @@ namespace Chain
  * walkers, which is useful for operations that need to look at all the walkers
  * in a given point, at once.
  * 
- * ChainStep is a BidirectionalIterator, you can increment and decrement it, as
- * well as dereference it. However, you must use prefix increment and decrement
- * post-fix increment and decrement are not supported.
+ * ChainStepIterator is a BidirectionalIterator, you can increment and decrement
+ * it, as well as dereference it. However, you must use prefix increment and
+ * decrement post-fix increment and decrement are not supported.
  */
 template <class ParamType, int BlockSize>
 class ChainStepIterator
 {
 public:
     /*!
-     * \brief Chain Constructor
+     * \brief ChainStepIterator Constructor
      * \param block The block the iterator is starting pointed to
      * \param blockStep The step within the block that the iterator starts pointing to
      */
     ChainStepIterator(ChainBlock<ParamType, BlockSize>* block, int blockStep):curr(block), stepIndex(blockStep){}
     ~ChainStepIterator(){}
     
+    /*!
+     * \brief operator== equality operator to test equality of two iterators (equivalence of their locations)
+     * \param rhs the second iterator (the right hand side)
+     * \return  True if they are equal, false otherwise
+     */
     bool operator==(const ChainStepIterator& rhs){return ((curr == rhs.curr) && (stepIndex == rhs.stepIndex));}
+    /*!
+     * \brief operator!= equality operator to test inequality of two iterators (inequivalence of their locations)
+     * \param rhs the second iterator (the right hand side)
+     * \return  True if they are not equal, false otherwise
+     */
     bool operator!=(const ChainStepIterator& rhs){return ((curr != rhs.curr) || (stepIndex != rhs.stepIndex));}
     
+    /*!
+     * \brief operator++ Prefix increment of the iterator, move it to the next step in the chain
+     * \return The iterator that was incremented
+     */
     ChainStepIterator<ParamType, BlockSize> operator++();
+    /*!
+     * \brief operator++ Prefix decrement of the iterator, move it to the previous step in the chain
+     * \return The iterator that was decremented
+     */
     ChainStepIterator<ParamType, BlockSize> operator--();
     
+    /*!
+     * \brief operator* Dereference the iterator to get a pointer to the walker parameter array for this step
+     * \return A pointer into the walker parameter array, pointed to the beginning of this step
+     */
     ParamType* operator*(){return (curr->chainArray + stepIndex*(curr->cellsPerStep));}
     
 private:
@@ -70,6 +92,13 @@ private:
 template <class ParamType, int BlockSize>
 ChainStepIterator<ParamType, BlockSize> ChainStepIterator<ParamType, BlockSize>::operator++()
 {
+    /*!
+     * @remark This function will allow the iterator to be incremented past the
+     * end. In fact, if the end is not the last cell of the last block merely a
+     * a cell in the last block, then it is possible to increment many times past
+     * the end iterator, once the iterator has gone past the last cell of the
+     * last block, the iterator is not recoverable
+     */
     //check if we are not at the end of a block
     if(stepIndex < (BlockSize-1))
     {//not at the end of a block, easy peasy
@@ -86,6 +115,10 @@ ChainStepIterator<ParamType, BlockSize> ChainStepIterator<ParamType, BlockSize>:
 template <class ParamType, int BlockSize>
 ChainStepIterator<ParamType, BlockSize> ChainStepIterator<ParamType, BlockSize>::operator--()
 {
+    /*!
+     * @remark This function will allow a .begin() operator to be decremented,
+     * once this happens the iterator is not recoverable
+     */
     //check if we are not at the end of a block
     if(stepIndex > 0)
     {//not at the start of a block, easy peasy
