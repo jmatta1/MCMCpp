922aff07fa1afc55a7f53e55beb20d1c2e1d4877
James Till Matta <jamesmatta@gmail.com>
2017-10-13 10:32:21 -0400

    Started documenting the sequential ensemblesampler
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- EnsembleSampler.h
+++ EnsembleSampler.h
@@ -20,7 +20,7 @@
 #include"Walker/Movers/WalkMove.h"
 #include"Walker/Walker.h"
 #include"Utility/GwDistribution.h"
-#include"Utility/NoStepEndAction.h"
+#include"Utility/UserOjbectsTest.h"
 
 namespace MarkovChainMonteCarlo
 {
@@ -55,22 +55,106 @@ template<class ParamType,
 class EnsembleSampler
 {
 public:
-    EnsembleSampler(int numWalker, int numParameter, unsigned long long maxChainSize);
-    ~EnsembleSampler();
+    //define some useful typenames
+    using ChainType=Chain::Chain<ParamType, BlockSize>;
+    using MoverType=Mover<ParamType, BlockSize, CustomDistribution, LikelihoodCalculator>;
+    using WalkerType=Walker::Walker<ParamType, BlockSize, MoverType, LikelihoodCalculator>;
+    using PrngType=Utility::MultiSampler<ParamType, CustomDistribution>;
+    using EnsembleSamplerType=EnsembleSampler<ParamType, LikelihoodCalculator, PostStepAction, PostBlockAction, Mover, CustomDistribution>;
+    //perform static checks of the users classes to ensure that they have the needed member functions for their role
+    static_assert(Utility::CheckPerformAction<PostStepAction, void, EnsembleSamplerType>(),
+                  "The PostStepAction class does not have the necessary member function with signature:\n"
+                  "  'void PerformAction(EnsembleSampler<ParamType, LikelihoodCalculator, PostStepAction,"
+                  "                                      PostBlockAction, Mover, CustomDistribution>*)'");
+    static_assert(Utility::CheckPerformAction<PostBlockAction, void, EnsembleSamplerType>(),
+                  "The PostBlockAction class does not have the necessary member function with signature:\n"
+                  "  'void PerformAction(EnsembleSampler<ParamType, LikelihoodCalculator, PostStepAction,\n"
+                  "                                      PostBlockAction, Mover, CustomDistribution>*)'");
+    static_assert(std::is_trivially_constructible<CustomDistribution>::value, "The custom distribution needs to be trivially constructible");
+    /*!
+     * \brief EnsembleSampler Constructs the ensemble sampler
+     * \param runNumber RunNumber, used to seed the random number generator
+     * \param numWalker The number of walkers to use
+     * \param numParameter The number of parameters in the problem
+     * \param maxChainSizeBytes The maximum size of the sample chain in bytes
+     */
+    EnsembleSampler(int runNumber, int numWalker, int numParameter, unsigned long long maxChainSizeBytes);
     
+    /*!
+     * @brief ~EnsembleSampler Delete the walker list then allow the rest to die normally
+     */
+    ~EnsembleSampler(){delete[] walkerList;}
+    
+    /*!
+     * \brief setInitialWalkerPos Gives an initial position to every walker.
+     * \param positions An array of floating point types with length numParameters*numWalker
+     */
     void setInitialWalkerPos(ParamType* positions);
     
+    /*!
+     * \brief runMCMC Runs the Markov chain Monte Carlo for a set number of samples
+     * \param numSamples The number of samples to store
+     * 
+     * In normal sampling mode, this function will run the ensemble for numSamples per walker
+     * In subSampling mode, this function will run the ensemble numSamples*subSampingInterval and store numSamples
+     */
     void runMCMC(int numSamples);
     
+    /*!
+     * \brief setSamplingMode Is used to change the sampling mode, either using subsampling, or taking all samples
+     * \param useSubSampling Boolean for whether or not to use subsampling, default is false
+     */
+    void setSamplingMode(bool useSubSampling=false, int subSampingInt=1);
+    
+    /*!
+     * \brief getParamSetIttBegin Gets an iterator pointing to the beginning of the chain
+     * that traverses the chain individual parameter set by individual parameter set. Incrementing
+     * the iterator gets you the parameter set of the next walker, not paying attention to which parameter set
+     * belongs to which step
+     * \return A Parameter Set iterator pointed to the beginning of the chain.
+     */
     Chain::ChainPsetIterator<ParamType, BlockSize> getParamSetIttBegin();
+    /*!
+     * \brief getParamSetIttEnd Gets a parameter set iterator pointing to the end of the chain
+     * \return A parameter set iterator that points just past the end of the chain
+     */
     Chain::ChainPsetIterator<ParamType, BlockSize> getParamSetIttEnd();
     
+    /*!
+     * \brief getStepIttBegin Gets a step iterator pointing to the chain step by step.
+     * Incrementing the iterator takes you to the first walker of the next step
+     * \return A Step iterator pointed to the beginning of the chain
+     */
     Chain::ChainStepIterator<ParamType, BlockSize> getStepIttBegin();
+    /*!
+     * \brief getStepIttEnd Gets a step iterator that points to the end of chain
+     * \return A Step iterator pointed to the end of the chain
+     */
     Chain::ChainStepIterator<ParamType, BlockSize> getStepIttEnd();
     
-    double getParameterAutoCorrelationTime(int paramIndex, int minAutoCorrTimes=10, int step=1, int loWin=10, int hiWin=10000, bool fast=false);
+    /*!
+     * \brief getAutoCorrelationTimes Calculates the correlation time for a given parameter
+     * \param paramIndex The index of the parameter [0, numParameter)
+     * \param minAutoCorrTimes The minimum required number of autocorrelation times the algorithm needs to examine
+     * \param step The increase in window size for each iteration of the algorithm
+     * \param loWin The minimum window size
+     * \param hiWin The maximum window size
+     * \param fast If true, only use the first power of two samples to accellerate calculation using FFT
+     * \return The autocorrelation time in samples for parameter # paramIndex
+     */
+    ParamType getAutoCorrelationTimes(int paramIndex, int minAutoCorrTimes=10, int step=1, int loWin=10, int hiWin=10000, bool fast=false);
 private:
-
+    ChainType markovChain;
+    WalkerType* walkerList;
+    MoverType moveProposer;
+    PrngType prng;
+    
+    int numParams;
+    int numWalkers;
+    int subSampingInterval=1;
+    
+    bool isInitted = false;
+    bool subSampling = false;
 };
 
 }
