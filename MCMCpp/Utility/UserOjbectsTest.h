22267b499c2f65faa584060f26e8370a1aca0951
James Till Matta <jamesmatta@gmail.com>
2017-10-20 20:02:34 -0400

    Added the test for the move update
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- MCMCpp/Utility/UserOjbectsTest.h
+++ MCMCpp/Utility/UserOjbectsTest.h
@@ -105,6 +105,25 @@ static auto testSignature_functor(int) -> std::false_type;
 /*!
  * @endcond
  */
+
+/*!
+ * @brief testSignature_PerformAction tests the existance of operator() that has 1 argument
+ * @tparam TestClass The class to test for existence of a member function
+ * @tparam RetType The type of the return argument
+ * @tparam Arg0 The type of the first function argument
+ */
+template<class TestClass, class RetType, class Arg0, class Arg1, class Arg2, class Arg3>
+static auto testSignature_udateWalker(unsigned long long) -> typename SameType<RetType,
+    decltype(std::declval<TestClass>().updateWalker(std::declval<Arg0>(), std::declval<Arg1>(), std::declval<Arg2>(), std::declval<Arg3>()))>::ValType;
+/*!
+ * @cond HIDDEN_SYMBOLS
+ * substitution failure branch, either the function does not exist or the arguments passed cannot be coerced into being the correct arguments
+ */
+template<class, class, class >
+static auto testSignature_udateWalker(int) -> std::false_type;
+/*!
+ * @endcond
+ */
 }
 
 /*!
@@ -124,6 +143,12 @@ struct CheckFunctor : decltype( Detail::testSignature_functor<TestClass, RetVal,
  */
 template<class TestClass, class RetVal, class Arg0>
 struct CheckCalcLogPostProb : decltype( Detail::testSignature_functor<TestClass, RetVal, Arg0>(0ULL) ){};
+
+/*!
+ * @brief Outer Function to perform test for calcLogPostProb function
+ */
+template<class TestClass, class RetVal, class Arg0, class Arg1, class Arg2, class Arg3>
+struct CheckCalcUpdateWalker : decltype( Detail::testSignature_udateWalker<TestClass, RetVal, Arg0, Arg1, Arg2>(0ULL) ){};
 }
 }
 #endif  //MCMC_UTILITY_USEROBJECTTEST_H
