27f80b94c6f55b2ec8d16cfa3e1f1115558657aa
James Till Matta <jamesmatta@gmail.com>
2017-11-02 11:00:55 -0400

    Special deleter for shared_ptrs to delete arrays
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ MCMCpp/Utility/ArrayDeleter.h
@@ -0,0 +1,47 @@
+/*!*****************************************************************************
+********************************************************************************
+**
+** @copyright Copyright (C) 2017 James Till Matta
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** 
+********************************************************************************
+*******************************************************************************/
+#ifndef MCMC_UTILITY_ARRAYDELETER_H
+#define MCMC_UTILITY_ARRAYDELETER_H
+// includes for C system headers
+// includes for C++ system headers
+// includes from other libraries
+// includes from MCMC
+
+namespace MarkovChainMonteCarlo
+{
+
+namespace Utility
+{
+
+/**
+ * @class ArrayDeleter
+ * @ingroup Utility
+ * @brief A custom deleter for shared pointers that own arrays
+ * @author James Till Matta
+ * 
+ * \tparam ArrayType The datatype the array is made of
+ * 
+ * This class simply provides the correct deleter to shared pointer. It is needed
+ * for C++11 through C++14, in C++17 using the syntax std::shared_ptr<ParamType[]>
+ * removes the need for this deleter
+ */
+template <class ArrayType>
+class ArrayDeleter
+{
+public:
+    void operator()(ArrayType const* arrPtr){delete[] arrayPtr;}
+private:
+};
+
+}
+}
+#endif  //MCMC_UTILITY_ARRAYDELETER_H
