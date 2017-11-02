907729cbfdb6b0158b2afd25e2e3da94fe6ee19e
James Till Matta <jamesmatta@gmail.com>
2017-11-02 12:13:49 -0400

    Added needed walker initialization
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- tests/SeqAcTime/src/main.cpp
+++ tests/SeqAcTime/src/main.cpp
@@ -8,25 +8,41 @@ namespace Analysis=MarkovChainMonteCarlo::Analysis;
 
 int main()
 {
-    float offsets[5] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-    float phis[5] = {0.8f,  // AC = 9
+    const int numWalkers = 100;
+    const int numParams = 5;
+    float offsets[numParams] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+    float phis[numParams] = {0.8f,  // AC = 9
                      0.904761904762f,  // AC = 20
                      0.9354838709677f,  // AC = 30
                      0.9672131147541f,  // AC = 60
                      0.990050200903734685f};  // AC = 200
-    float vars[5] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-    Mover::AutoRegressiveMove<float> mover(5, offsets, phis, vars);
-    EnsembleSampler<float, Mover::AutoRegressiveMove<float> > sampler(0, 100, 5, mover);
+    float vars[numParams] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+    
+    std::cout<<"Building initial mover"<<std::endl;
+    Mover::AutoRegressiveMove<float> mover(numParams, offsets, phis, vars);
+    
+    std::cout<<"Getting initial values"<<std::endl;
+    float* initVals = new float[numWalkers*numParams];
+    float* auxVals = new float[numWalkers];
+    mover.getInitialPoints(initVals, auxVals, numWalkers);
+    
+    std::cout<<"Building sampler"<<std::endl;
+    EnsembleSampler<float, Mover::AutoRegressiveMove<float> > sampler(0, numWalkers, numParams, mover);
+    std::cout<<"Setting initial values"<<std::endl;
+    sampler.setInitialWalkerPos(initVals, auxVals);
+    
     std::cout<<"Running ensemble sampler for autoregressive model"<<std::endl;
     sampler.runMCMC(200000);
-    auto startItt = sampler.getStepIttBegin();
-    auto endItt = sampler.getStepIttEnd();
+    
     std::cout<<"Calculating integrated autocorrelation times for autoregressive model"<<std::endl;
     Analysis::AutoCorrCalc<float> acCalc(5, 100);
+    auto startItt = sampler.getStepIttBegin();
+    auto endItt = sampler.getStepIttEnd();
     acCalc.allAutoCorrTime(startItt, endItt, 200000);
-    std::cout<<"Paramater 0 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(0)<<" Actual: 9"<<std::endl;
-    std::cout<<"Paramater 1 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(1)<<" vs. 20"<<std::endl;
-    std::cout<<"Paramater 2 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(2)<<" vs. 30"<<std::endl;
-    std::cout<<"Paramater 3 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(3)<<" vs. 60"<<std::endl;
-    std::cout<<"Paramater 4 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(4)<<" vs. 200"<<std::endl;
+    
+    std::cout<<"Paramater 0 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(0)<<" Actual:   9"<<std::endl;
+    std::cout<<"Paramater 1 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(1)<<" Actual:  20"<<std::endl;
+    std::cout<<"Paramater 2 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(2)<<" Actual:  30"<<std::endl;
+    std::cout<<"Paramater 3 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(3)<<" Actual:  60"<<std::endl;
+    std::cout<<"Paramater 4 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(4)<<" Actual: 200"<<std::endl;
 }
