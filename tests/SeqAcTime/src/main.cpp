94e9428ac2ce27099daa8d754bd7072c509b13de
James Till Matta <jamesmatta@gmail.com>
2017-11-03 11:50:35 -0400

    Some debug output
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- tests/SeqAcTime/src/main.cpp
+++ tests/SeqAcTime/src/main.cpp
@@ -10,6 +10,7 @@ int main()
 {
     const int numWalkers = 100;
     const int numParams = 5;
+    const int numSteps = 200000;
     float offsets[numParams] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
     float phis[numParams] = {0.8f,  // AC = 9
                              0.904761904762f,  // AC = 20
@@ -32,17 +33,18 @@ int main()
     sampler.setInitialWalkerPos(initVals, auxVals);
     
     std::cout<<"Running ensemble sampler for autoregressive model"<<std::endl;
-    sampler.runMCMC(200000);
+    sampler.runMCMC(numSteps);
     
     std::cout<<"Calculating integrated autocorrelation times for autoregressive model"<<std::endl;
-    Analysis::AutoCorrCalc<float> acCalc(5, 100);
+    Analysis::AutoCorrCalc<float> acCalc(numParams, numWalkers);
     auto startItt = sampler.getStepIttBegin();
     auto endItt = sampler.getStepIttEnd();
-    acCalc.allAutoCorrTime(startItt, endItt, 200000);
+    acCalc.allAutoCorrTime(startItt, endItt, numSteps);
     
     std::cout<<"P0 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(0)<<" Actual:   9"<<std::endl;
     std::cout<<"P1 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(1)<<" Actual:  20"<<std::endl;
     std::cout<<"P2 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(2)<<" Actual:  30"<<std::endl;
     std::cout<<"P3 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(3)<<" Actual:  60"<<std::endl;
     std::cout<<"P4 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(4)<<" Actual: 200"<<std::endl;
+    std::cout<<"In Main dest"<<std::endl; 
 }
