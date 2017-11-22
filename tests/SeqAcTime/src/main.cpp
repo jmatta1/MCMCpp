1a2fbf73949ddf30e91c339e9277663848f3d91c
James Till Matta <jamesmatta@gmail.com>
2017-11-22 10:37:56 -0500

    Tweaks for sanity
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- tests/SeqAcTime/src/main.cpp
+++ tests/SeqAcTime/src/main.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 #include"Movers/Diagnostic/AutoRegressiveMove.h"
-#include"Movers/Diagnostic/SequenceMove.h"
 #include"Analysis/AutoCorrCalc.h"
 #include"EnsembleSampler.h"
-#include<fstream>
-using std::ofstream;
+
 using MarkovChainMonteCarlo::EnsembleSampler;
 namespace Mover=MarkovChainMonteCarlo::Mover;
 namespace Analysis=MarkovChainMonteCarlo::Analysis;
-
-static const char* FileNames[5]={"p0.csv", "p1.csv", "p2.csv", "p3.csv", "p4.csv"};
 
 int main()
 {
     const int runNumber = 0;
     const int numWalkers = 100;
     const int numParams = 5;
-    const int numSteps = 199999;
-    double offsets[numParams] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
-    double phis[numParams] = {0.8f,  // AC = 9
-                             0.904761904762f,  // AC = 20
-                             0.9354838709677f,  // AC = 30
-                             0.9672131147541f,  // AC = 60
-                             0.990050200903734685f};  // AC = 200
-    double vars[numParams] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+    const int numSteps = 262143;
+    double offsets[numParams] = {1.0, 1.0, 1.0, 1.0, 1.0};
+    double phis[numParams] = {0.8,  // AC = 9
+                             0.904761904762,  // AC = 20
+                             0.9354838709677,  // AC = 30
+                             0.9672131147541,  // AC = 60
+                             0.990050200903734685};  // AC = 200
+    double vars[numParams] = {1.0, 1.0, 1.0, 1.0, 1.0};
     
     std::cout<<"Building initial mover"<<std::endl;
     Mover::AutoRegressiveMove<double> mover(numParams, offsets, phis, vars);
@@ -37,6 +33,8 @@ int main()
     EnsembleSampler<double, Mover::AutoRegressiveMove<double> > sampler(runNumber, numWalkers, numParams, mover);
     std::cout<<"Setting initial values"<<std::endl;
     sampler.setInitialWalkerPos(initVals, auxVals);
+    delete[] initVals;
+    delete[] auxVals;
     
     std::cout<<"Running ensemble sampler for autoregressive model"<<std::endl;
     sampler.runMCMC(numSteps);
@@ -53,6 +51,4 @@ int main()
     std::cout<<"P2 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(2)<<" Actual:  30"<<std::endl;
     std::cout<<"P3 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(3)<<" Actual:  60"<<std::endl;
     std::cout<<"P4 Calculated AutoCorrelation Time: "<<acCalc.retrieveAutoCorrelationTime(4)<<" Actual: 200"<<std::endl;
-    delete[] initVals;
-    delete[] auxVals;
 }
