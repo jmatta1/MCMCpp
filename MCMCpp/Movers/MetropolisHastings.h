1826bf7de6d5e85dfc2c5dffdba7eefbb42209a5
James Till Matta <jamesmatta@gmail.com>
2018-02-16 11:53:17 -0500

    Rough cut of Metropolis Hastings mover
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- MCMCpp/Movers/MetropolisHastings.h
+++ MCMCpp/Movers/MetropolisHastings.h
@@ -55,15 +55,12 @@ public:
      * @param numParams The number of parameters to work with
      * @param prngInit The seed for the random number generator
      * @param orig The original calculator class that will be copied to make the one stored internally
-     * @param covarMat The covariance matrix for the drawn samples
      */
-    MetropolisHastings(int numParams, long long prngInit, const Calculator& orig, ParamType* covarMat):
+    MetropolisHastings(int numParams, long long prngInit, const Calculator& orig):
         decompCovMat(Utility::autoAlignedAlloc<ParamType>(sizeof(ParamType)*numParams*numParams), Utility::AlignedArrayDeleter<ParamType>()),
         proposal(Utility::autoAlignedAlloc<ParamType>(sizeof(ParamType)*numParams)),
         paramCount(numParams), prng(prngInit), calc(orig)
-    {
-        decomposeCovarMat(covarMat);
-    }
+    {setIdentityCovar();}
     
     ~MetropolisHastings(){Utility::delAAA(proposal);}
     
@@ -83,22 +80,41 @@ public:
      */
     MetropolisHastings<ParamType, Calculator>& operator=(const MetropolisHastings<ParamType, Calculator>& rhs) = delete;
     
-    /*!
-     * \brief Allows the user to override the default choice for the scaling factor gamma (2.38 / sqrt(2*numDim))
-     * \param newGamma The new value for gamma
-     * 
-     * According to the paper, for a multivariate normal target 2.38/sqrt(2*numDims) is about optimal, but
-     * this is not necessarily the optimal choice for different targets
-     */
-    void overrideGamma(ParamType newGamma){ gamma = newGamma;}
+    
     
     /*!
-     * \brief Set a new b for the part of the proposal that needs UniformRand(-b, b)
-     * \param newRand the new value for b
+     * @brief Allows the user to set a covariance matrix that is not the identity matrix
+     * @param cMat the pointer to the square matrix (of size paramCount*paramCount) containing the parameter covariances
+     * @return True if the matrix is accepted, False if the matrix failed certain tests or cholesky decomposition failed
      * 
-     * According to the paper, b can be quite small, infact in their numerical tests they used b = 10^-4 so that is my default
+     * The caller of this function retains ownership of the memory containing the covariance matrix and is responsible for deleting it
      */
-    void overrideRandBounds(ParamType newRand){smallRandLowEdge = -newRand; smallRandWidth = 2*newRand;}
+    bool setCovarMat(ParamType* cMat)
+    {
+        int result = testCovar(cMat);
+        if(result == 1)
+        {
+            if(!decomposeCovarMat(cMat))
+            {
+                setIdentityCovar();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if(result == 2)
+        {
+            setDiagonalDecomp(cMat);
+            return true;
+        }
+        else
+        {
+            setIdentityCovar();
+            return false;
+        }
+    }
     
     /*!
      * \brief setPrngSeed Sets the seed and stream number of the underlying prng
@@ -138,20 +154,184 @@ public:
     }
 
 private:
+    /*!
+     * \brief A function to draw a random vector with the given covariance matrix, placing it in in the proposal array
+     */
     void getCovarSample()
     {
-        
-        
+        if(diagonal) getDiagonalCovarSample();
+        //otherwise do the more complicated thing
+        //first draw paramCount independent standard normal distributed values
+        for(int i=0; i<paramCount; ++i)
+        {
+            proposal[i] = prng.getNormalReal();
+        }
+        //otherwise multiply them by the left triangular matrix from the Cholesky decomposition of the covariance matrix
+        ParamType* mat = decompCovMat.get();
+        for(int i=(paramCount-1); i>=0; --i)
+        {//do this loop in reverse order so we do not overwrite values that we need before we are done with them
+            int offset = (i*paramCount);
+            /*
+            //since this is a summation we *could* use kahan summation to make sure we don't lose precision for larger parameter counts
+            ParamType comp = 0.0;
+            ParamType sum = 0.0;
+            for(int j=i; j>=0; --j)
+            {
+                ParamType compTerm = ((proposal[i]*mat[offset+j]) - comp);
+                ParamType tempSum = (sum + compTerm);
+                comp = ((tempSum - sum) - compTerm);
+                sum = tempSum;
+            }
+            proposal[i] = sum;
+            //however this is probably unnecessary for the approximations inherent in an MCMC scheme anyways
+            */
+            proposal[i] *= mat[offset+i];
+            for(int j=(i-1); j>=0; --j)
+            {
+                proposal[i] += (proposal[j]*mat[offset+j]);
+            }
+        }
     }
 
-    void decomposeCovarMat(ParamType* cMat)
+    /*!
+     * @brief Calculates the sample in the simple case of a purely diagonal sample matrix, storing it in the proposal array
+     */
+    void getDiagonalCovarSample()
     {
-        
+        //draw the normals multiplied by the diagonal element and we are done
+        ParamType* mat = decompCovMat.get();
+        for(int i=0; i<paramCount; ++i)
+        {
+            proposal[i] = (prng.getNormalReal()*mat[paramCount*i+i]);
+        }
+    }
+    
+    /*!
+     * \brief Tests the covariance matrix for suitability
+     * \param cMat The covariance matrix to be tested.
+     * \return 0 for a matrix that is not symmetrix or has a non-positive value on the diagonal, 2 for a diagonal matrix that has only positive values, 1 in all other cases
+     */
+    int testCovar(ParamType* cMat)
+    {
+        bool isDiagonal = true;
+        for(int i=1; i<paramCount; ++i)
+        {
+            //test that all the diagonals are greater than zero
+            if(cMat[i*paramCount+i] <= 0.0) return 0;//zero or negative value in diagonal
+            //test that it is symmetric
+            for(int j=0; j<i; ++j)
+            {
+                ParamType val = cMat[i*paramCount+j];
+                if(val != cMat[j*paramCount+i]) return 0;//not symmetric
+                if(val != 0.0) isDiagonal = false;
+            }
+        }
+        //check if we discovered that it is diagonal
+        if(isDiagonal) return 2;
+        //it is not diagonal but passes the other tests
+        return 1;
+    }
+    
+    /*!
+     * @brief Perform a Cholesky decomposition of the (tested) covariance matrix and store the result
+     * @param cMat The covariance matrix to be decomposed
+     * @return True for successful decomposition, false for failure
+     */
+    bool decomposeCovarMat(ParamType* cMat)
+    {
+        ParamType* temp = decompCovMat.get();
+        for(int i=0; i<paramCount; ++i)
+        {
+            for(int j=0; j<paramCount; ++j)
+            {
+                if(i==j)//diagonal term case
+                {
+                    ParamType value = cMat[i*paramCount+j];
+                    for(int k=0; k<j; ++k)
+                    {
+                        value -= (temp[j*paramCount+k]*temp[j*paramCount+k]);
+                    }
+                    if(value >= 0.0)
+                    {
+                        temp[i*paramCount+j] = std::sqrt(value);
+                    }
+                    else//something broke return that the matrix was not valid
+                    {
+                        return false;
+                    }
+                }
+                else if(j<i)//non diagonal case with non-zero values
+                {
+                    ParamType value = cMat[i*paramCount+j];
+                    for(int k=0; k<j; ++k)
+                    {
+                        value -= (temp[i*paramCount+k]*temp[j*paramCount+k]);
+                    }
+                    temp[i*paramCount+j] = (value/temp[j*paramCount+j]);
+                }
+                else//non diagonal case were the terms are zero
+                {
+                    temp[i*paramCount+j] = 0.0;
+                }
+            }
+        }
+        diagonal = false;
+        return true;
+    }
+    
+    /*!
+     * \brief When the covaraiance matrix is diagonal, this merely sets the decomposed matrix to the square roots of the covar matrices elements
+     * \param cMat the diagonal covariance matrix
+     */
+    void setDiagonalDecomp(ParamType* cMat)
+    {
+        ParamType* temp = decompCovMat.get();
+        for(int i=0; i<paramCount; ++i)
+        {
+            int offset = i*paramCount;
+            for(int j=0; j<paramCount; ++j)
+            {
+                if(i==j)
+                {
+                    temp[offset + j] = std::sqrt(cMat[offset + j]);
+                }
+                else
+                {
+                    temp[offset + j] = 0.0;
+                }
+            }
+        }
+        diagonal = true;
+    }
+    
+    /*!
+     * \brief Sets the decomposition matrix to the identity matrix (used as a default for bad covar matrices)
+     */
+    void setIdentityCovar()
+    {
+        ParamType* temp = decompCovMat.get();
+        for(int i=0; i<paramCount; ++i)
+        {
+            int offset = i*paramCount;
+            for(int j=0; j<paramCount; ++j)
+            {
+                if(i==j)
+                {
+                    temp[offset + j] = 1.0;
+                }
+                else
+                {
+                    temp[offset + j] = 0.0;
+                }
+            }
+        }
+        diagonal = true;
     }
     
     std::shared_ptr<ParamType> decompCovMat;
     ParamType* proposal = nullptr;
     int paramCount;
+    bool diagonal = true;
     Utility::MultiSampler<ParamType, DistType> prng;
     Calculator calc;
 };
