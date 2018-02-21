fbd28ce5c7211e1a99173645745485869eb9b5e6
James Till Matta <jamesmatta@gmail.com>
2018-02-21 12:49:15 -0500

    Finished implementing percentile and peak finder
    Closes #4
    Closes #21
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- MCMCpp/Analysis/PercentileAndMaximumFinder.h
+++ MCMCpp/Analysis/PercentileAndMaximumFinder.h
@@ -14,6 +14,10 @@
 // includes for C system headers
 // includes for C++ system headers
 #include<limits>//for numeric_limits
+#include<cassert>//for assert
+#include<sstream>//for generating file names for saving histograms
+#include<fstream>//for saving histograms
+#include<string>//for passing strings around for file names
 // includes from other libraries
 // includes from MCMCpp
 #include"../Utility/Misc.h"
@@ -65,21 +69,21 @@ public:
      * \param end The step iterator at the end of the chain
      * \param sliceInterval The relative index of sample to use, must be >= 1. a value of 1 uses every sample, 2 uses every other sample, so on and so forth
      */
-    void calculateHistograms(IttType start, IttType end, int sliceInterval=1);
+    void processChainData(IttType start, IttType end, int sliceInterval=1);
     
     /*!
      * \brief Takes a given parameter and value and finds what the percentile for that value is
      * \param pIndex The index of the parameter to find the percentile for
      * \param val The value for which a percentile desired
-     * \return The percentile of the value passed if it is within range, otherwise, -1
+     * \return The percentile of the value passed (in the range [0,100]) if it is within range, otherwise, -1
      */
     ParamType getPercentileFromValue(int pIndex, const ParamType& val);
     
     /*!
      * \brief Takes a given percentile and finds the value corresponding to that percentile
      * \param pIndex The index of the parameter to find the value for
-     * \param val The percentile desired
-     * \return The value at the percentile passed if percentile is on [0,100], otherise the minumum value of the range is returned
+     * \param val The percentile desired (in the range [0,100])
+     * \return The value at the percentile passed if percentile is on [0,100], otherise the minumum value of the range minus 1e4 is returned
      */
     ParamType getValueFromPercentile(int pIndex, const ParamType& per);
     
@@ -103,8 +107,28 @@ public:
      * \return The top of a parameter's range of values
      */
     ParamType getParamMaximum(int pIndex){return (paramBounds[2*pIndex]+(paramBounds[2*pIndex+1]*bCount));}
+    
+    /*!
+     * \brief Writes the histograms, normal and cumulative sums, to files whose names start with fileNameBase
+     * \param fileNameBase The main component of the file name to which sub identifiers are prefixed on
+     */
+    void writeHistogramsInCsvFormat(const std::string& fileNameBase);
 
 private:
+    /*!
+     * \brief Writes the specified histogram into csv format
+     * \param histNum The parameter number of the histogram
+     * \param fileNameBase The base filename from which the output filename will be calculated
+     */
+    void write1dHistCsv(int histNum, const std::string& fileNameBase);
+    
+    /*!
+     * \brief Writes the specified cumulative sum into csv format
+     * \param histNum The parameter number of the cumulative sum
+     * \param fileNameBase The base filename from which the output filename will be calculated
+     */
+    void write1dCumSumCsv(int histNum, const std::string& fileNameBase);
+    
     /*!
      * \brief Calculates the cumulative sums from the histograms
      */
@@ -145,6 +169,8 @@ private:
         return (ParamType(0) < val) - (val < ParamType(0));
     }
     
+    
+    
     const ParamType expandFraction = static_cast<ParamType>(1.001); ///<Holds the margin for expanding negative bounds downward and positive bounds upward
     const ParamType contractFraction = static_cast<ParamType>(0.999);///<Holds the margin for expanding negative bounds *upward* and positive bounds downward
     const ParamType minSize = static_cast<ParamType>(0.001);///<Holds the minimum magnitude of a range
@@ -152,7 +178,7 @@ private:
     int* hists = nullptr;///<Holds a histogram of each parameter
     int* cumSum = nullptr;///<Holds a cumulative sum of each histogram
     ParamType* paramBounds = nullptr; ///<Holds the min and max of every parameter
-    int numPoints;///<Holds the total number of points that went into generating the histograms
+    int numPoints=0;///<Holds the total number of points that went into generating the histograms
     int pCount;///<Holds the number of parameters per point
     int wCount;///<Holds the number of walkers per step
     int bCount;///<Holds the number of bins per histogram
@@ -164,16 +190,21 @@ template<class ParamType>
 PercentileAndMaximumFinder<ParamType>::PercentileAndMaximumFinder(int numParams, int numWalkers, int binsPerAxis):
     pCount(numParams), wCount(numWalkers), bCount(binsPerAxis), cbCount(binsPerAxis+1)
 {
+    assert(pCount > 0);
+    assert(wCount > 0);
+    assert(bCount > 1);
     //allocate the array of hists
     hists = new int[bCount*pCount];
     //allocate the array of cumulative sums
     cumSum = new int[cbCount*pCount];
     //allocate the set of parameter bounds and set them to extreme values
     paramBounds = new ParamType[2*pCount];
+    //preemptively zero everything
+    zeroStorage();
 }
 
 template<class ParamType>
-void PercentileAndMaximumFinder<ParamType>::calculateHistograms(IttType start, IttType end, int sliceInterval)
+void PercentileAndMaximumFinder<ParamType>::processChainData(IttType start, IttType end, int sliceInterval)
 {
     //reset our storage
     zeroStorage();
@@ -191,6 +222,7 @@ void PercentileAndMaximumFinder<ParamType>::calculateHistograms(IttType start, I
         //now iterate through every point
         for(int i=0; i<wCount; ++i)
         {
+            ++numPoints;
             handlePoint( ((*itt)+(i*pCount)) );
         }
         ++index;
@@ -203,19 +235,167 @@ void PercentileAndMaximumFinder<ParamType>::calculateHistograms(IttType start, I
 template<class ParamType>
 ParamType PercentileAndMaximumFinder<ParamType>::getPercentileFromValue(int pIndex, const ParamType& val)
 {
-    
+    if((!binned) ||  //the caller has not put in data
+       (val<paramBounds[2*pIndex]) || //the value is below the range of parameters encountered
+       (val>(paramBounds[2*pIndex]+paramBounds[2*pIndex+1]*(bCount+1)))) //the value is greater than the range of parameters encountered
+    {
+        return static_cast<ParamType>(-1);
+    }
+    //now translate the value back into a bin number
+    int binNum = ((val - paramBounds[2*pIndex])/paramBounds[2*pIndex+1]);
+    int cbinNum = (binNum + 1);
+    //now linearly interpolate along the cumsum histogram to find the exact number of entries corresponding to that value
+    //between the current location and the one before it to find the value
+    //the value of the cell represents the value at the top edge of the cell,
+    //the x values are then the high edge of the cell before and the current cell
+    //the cell edge formula from paramBounds of binNum*binWidth+lowestBinEdge
+    //gives the low edge for a bin, to get the high edge, add 1 to the bin number
+    //however, the bin numbering is for hists, the cumSum arrays are offset up by
+    //one bin, so subtract 1 from the bin number to get the high edge of the correct bin
+    ParamType x1 = (((cbinNum-1)*paramBounds[2*pIndex+1])+paramBounds[2*pIndex]);
+    ParamType x2 = ((cbinNum*paramBounds[2*pIndex+1])+paramBounds[2*pIndex]);
+    ParamType y1 = cumSum[cbinNum-1];
+    ParamType y2 = cumSum[cbinNum];
+    //we have x1,y1 and x2,y2 so we can now find the slope and intercept of a straight-line connecting them
+    ParamType m = ((y2-y1)/(x2-x1));
+    ParamType b = (((x2*y1)-(x1*y2))/(x2-x1));
+    //then the value we extract is the value the user wanted and simply comes from plugging in entries into the line formula
+    ParamType entries = static_cast<int>((val*m)+b);
+    //now we find the percentile by ratio
+    return (static_cast<ParamType>(100)*(entries/static_cast<ParamType>(numPoints)));
 }
 
 template<class ParamType>
 ParamType PercentileAndMaximumFinder<ParamType>::getValueFromPercentile(int pIndex, const ParamType& per)
 {
-    
+    if((!binned) || (0>per) || (100<per)) return (paramBounds[2*pIndex] - 1e4);
+    //first find out how many entries the given percentile is:
+    int entries = (per/static_cast<ParamType>(100))*static_cast<ParamType>(numPoints);
+    //get the cumulative sum array for the parameter
+    int* cs = (cumSum + pIndex*cbCount);
+    //do a binary search in the array to find the cell that contains the value (or the first cell whose value is above it)
+    int first = 0;
+    int last = (cbCount-1);
+    if(entries == 0)
+    {
+        first = 1;
+        last = 1;
+    }
+    else
+    {
+        while(first != last)
+        {
+            int mid = ((first + last)/2);
+            if(cs[mid] >= entries)
+            {//since cell 0 always contains 0 this condition cannot be true if mid = 0;
+                if(cs[mid-1] <= entries)//if this is true we are at the correct cell
+                {
+                    first = mid;
+                    last = mid;
+                }
+                else
+                {//mid is too high a cell, try again
+                    last = mid;
+                }
+            }
+            else
+            {// if we are here then mid was too low a cell, try again
+                first = mid;
+            }
+        }
+    }
+    //now both first and last point to the correct location, do a linear interpolation
+    //between the current location and the one before it to find the value
+    //the value of the cell represents the value at the top edge of the cell,
+    //the y values are then the high edge of the cell before and the current cell
+    //the cell edge formula from paramBounds of binNum*binWidth+lowestBinEdge
+    //gives the low edge for a bin, to get the high edge, add 1 to the bin number
+    //however, the bin numbering is for hists, the cumSum arrays are offset up by
+    //one bin, so subtract 1 from the bin number to get the high edge of the correct bin
+    ParamType x1 = cs[last-1];
+    ParamType x2 = cs[last];
+    ParamType y1 = (((last-1)*paramBounds[2*pIndex+1])+paramBounds[2*pIndex]);
+    ParamType y2 = ((last*paramBounds[2*pIndex+1])+paramBounds[2*pIndex]);
+    //we have x1,y1 and x2,y2 so we can now find the slope and intercept of a straight-line connecting them
+    ParamType m = ((y2-y1)/(x2-x1));
+    ParamType b = (((x2*y1)-(x1*y2))/(x2-x1));
+    //then the value we extract is the value the user wanted and simply comes from plugging in entries into the line formula
+    return ((entries*m)+b);
 }
 
 template<class ParamType>
 ParamType PercentileAndMaximumFinder<ParamType>::getValueOfPeak(int pIndex)
 {
-    
+    if(!binned) return (paramBounds[2*pIndex] - 1e4);
+    //jump to the histogram for the given parameter
+    int* hist = (hists + pIndex*bCount);
+    //find the bin with the largest content
+    int binMax = 0;
+    int maxVal = -1;
+    for(int i=0; i<bCount; ++i)
+    {
+        if(hist[i] > maxVal)
+        {
+            maxVal = hist[i];
+            binMax = i;
+        }
+    }
+    //now translate that bin number into a bin center
+    return (((binMax+0.5)*paramBounds[2*pIndex+1])+paramBounds[2*pIndex]);
+}
+
+template<class ParamType>
+void PercentileAndMaximumFinder<ParamType>::writeHistogramsInCsvFormat(const std::string& fileNameBase)
+{
+    for(int i=0; i<pCount; ++i)
+    {
+        write1dHistCsv(i, fileNameBase);
+        write1dCumSumCsv(i, fileNameBase);
+    }
+}
+
+template<class ParamType>
+void PercentileAndMaximumFinder<ParamType>::write1dHistCsv(int histNum, const std::string& fileNameBase)
+{
+    std::ostringstream namer;
+    namer<<fileNameBase<<"_p"<<histNum<<".csv";
+    std::ofstream out;
+    out.open(namer.str().c_str());
+    out<<"# Lines starting with a '#' in the first column are ignored\n";
+    out<<"# X-axis: nbins, first bin low edge, last bin high edge\n";
+    //write the axis parameters
+    out<<bCount<<", "<<paramBounds[2*histNum]<<", "<<(paramBounds[2*histNum] + (bCount*paramBounds[2*histNum+1]))<<"\n";
+    int offset = histNum*bCount;
+    out<<"# bin number, value\n";
+    //write the bin data
+    for(int i=0; i<bCount; ++i)
+    {
+        out<<i<<", "<<hists[offset+i]<<"\n";
+    }
+    out<<std::flush;
+    out.close();
+}
+
+template<class ParamType>
+void PercentileAndMaximumFinder<ParamType>::write1dCumSumCsv(int histNum, const std::string& fileNameBase)
+{
+    std::ostringstream namer;
+    namer<<fileNameBase<<"_cs_p"<<histNum<<".csv";
+    std::ofstream out;
+    out.open(namer.str().c_str());
+    out<<"# Lines starting with a '#' in the first column are ignored\n";
+    out<<"# X-axis: nbins, first bin low edge, last bin high edge\n";
+    //write the axis parameters
+    out<<bCount<<", "<<(paramBounds[2*histNum]-paramBounds[2*histNum+1])<<", "<<(paramBounds[2*histNum] + (bCount*paramBounds[2*histNum+1]))<<"\n";
+    int offset = histNum*cbCount;
+    out<<"# bin number, value\n";
+    //write the bin data
+    for(int i=0; i<cbCount; ++i)
+    {
+        out<<i<<", "<<cumSum[offset+i]<<"\n";
+    }
+    out<<std::flush;
+    out.close();
 }
 
 template<class ParamType>
@@ -319,6 +499,8 @@ void PercentileAndMaximumFinder<ParamType>::testPtExtremes(ParamType* pt)
 template<class ParamType>
 void PercentileAndMaximumFinder<ParamType>::zeroStorage()
 {
+    //reset the point counter
+    numPoints = 0;
     //zero all the histograms
     int arrSize = bCount*pCount;
     for(int i=0; i<arrSize; ++i)
@@ -337,7 +519,10 @@ void PercentileAndMaximumFinder<ParamType>::zeroStorage()
         paramBounds[2*i] = std::numeric_limits<ParamType>::max();
         paramBounds[2*i+1] = std::numeric_limits<ParamType>::min();
     }
+    binned = false;
 }
 
 }
 }
+
+#endif //MCMCPP_ANALYSIS_PERCENTILES_H
